96a74843577f5f8cc9c58a60f86f8b1f1771835e
mohiu-bhai <mohiuddintamim999@gmail.com>
2022-01-02 21:39:32 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "app/link.php"?>
+    <title>S$P 500</title>
+</head>
+<body>
+    <h1>Tamim o,S</h1>
+</body>
+</html>
