041e9c306457621fbf9c224387b820f3864142c5
mohiu-bhai <mohiuddintamim999@gmail.com>
2022-01-04 19:40:43 +0600

    1

--- app/link.php
+++ app/link.php
@@ -1,2 +1,4 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/33be89dc88.js" crossorigin="anonymous"></script>
+<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="css/style.css">
--- index.php
+++ index.php
@@ -5,9 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "app/link.php"?>
-    <title>S$P 500</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>S$P 400</title>
 </head>
 <body>
-    <h1>Tamim o,S</h1>
+    <div class="align-middle root-section">
+        <div class="card">
+            <h2>Mohiuddin</h2>
+        </div>
+        <div class="card">
+            <form action="" class="row g-1">
+                <div class="col-md-6">
+                    <label class="form-label" for="firstName">First Name</label>
+                    <input class="form-control" type="text" id="firstName" placeholder="First name">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label" for="lastName">Last Name</label>
+                    <input class="form-control" type="text" id="lastName" placeholder="Last Name">
+                </div>
+                <div class="col-md-12">
+                    <label class="form-label" for="email">Email</label>
+                    <input class="form-control" type="email" id="email" placeholder="example@email.com">
+                </div>
+                <br>
+                <div class="row-md">
+                    <label class="form-label" for="userName">User Name</label>
+                    <input class="form-control" type="text" id="userName" placeholder="@username">
+                </div>
+                <div class="col-md-4">
+                    <label class="form-label" for="dateOfBirth">Date Of Birth</label>
+                    <input class="form-control" type="date" id="dateOfBirth" placeholder="example@email.com">
+                </div>
+                <div>
+                    <div class="col-md-6">
+                        <label class="form-label" for="password">Password</label>
+                        <input class="form-control" type="password" id="password" placeholder="Password">
+                    </div>
+                    <div class="col-md-6">
+                        <label class="form-label" for="confPassword">Confirm Password</label>
+                        <input class="form-control" type="password" id="confPassword" placeholder="Confirm Password">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
